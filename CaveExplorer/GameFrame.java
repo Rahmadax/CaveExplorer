1a5530f13e718f26b16e0e22e5eb06a08797a625
Ollie Rahman <ollierahman@gmail.com>
2017-01-13 17:06:18 +0000

    Version 0.0.04
    Started making the game graphical. Built the basic Menu Jframe.
    
    Added button listener.

---
+++ CaveExplorer/GameFrame.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GameFrame {
+   
+   private static class HelloWorldDisplay extends JPanel {
+      public void paintComponent(Graphics g) {
+         super.paintComponent(g);
+         g.drawString( "Hello World!", 100, 30 );
+      }
+   }
+   
+   private static class ButtonHandler implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         System.exit(0);
+      }
+   }
+   
+   public static void main(String[] args) {
+      
+      HelloWorldDisplay displayPanel = new HelloWorldDisplay();
+      JButton ButtonPlay = new JButton("Play");
+	  JButton ButtonGM = new JButton("Choose Game Mode");
+	  JButton ButtonExit = new JButton("Exit");
+      ButtonHandler listener = new ButtonHandler();
+      ButtonPlay.addActionListener(listener);
+	  
+	  ButtonPlay.setBounds(50,100,300,30);
+	  ButtonGM.setBounds(50,175,300,30);
+	  ButtonExit.setBounds(50,250,300,30);
+	  
+
+      JPanel content = new JPanel();
+      content.setLayout(new BorderLayout());
+      content.add(displayPanel, BorderLayout.CENTER);
+      content.add(ButtonPlay);
+	  content.add(ButtonGM);
+	  content.add(ButtonExit);
+	  content.add(new JLabel(new ImageIcon("C:/OllieJava/CaveExplorer/knight.jpg")));
+
+
+      JFrame window = new JFrame("Cave Explorer");
+      window.setContentPane(content);
+      window.setSize(500,500);
+      window.setLocation(100,100);
+      window.setVisible(true);
+
+   }
+   
+}
